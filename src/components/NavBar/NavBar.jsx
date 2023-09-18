5671ce5eecc3e534e8ceec51e23de022bb23e757
Tawkir711 <mdtawkirk711@gmail.com>
2023-09-18 10:22:03 +0600

    added menu icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../link/Link';
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const NavBar = () => {
+  const [open, setOpen] =useState(false)
   const routes = [
     {id: 1, path:'/home', name: 'Home'},
     {id: 2, path:'/about', name: 'About'},
@@ -11,6 +13,13 @@ const NavBar = () => {
   ]
   return (
     <nav>
+      <div className="md:hidden text-2xl" onClick={() => setOpen(!open)}>
+        {open === true ? (
+          <AiOutlineClose></AiOutlineClose>
+        ) : (
+          <AiOutlineMenu></AiOutlineMenu>
+        )}
+      </div>
       <ul className="md:flex">
         {routes.map((route) => (
           <Link key={route.id} route={route}></Link>
